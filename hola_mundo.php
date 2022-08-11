2ef40d8cd95b2d2552b0cbc5aeac5319219dfb8a
Guillermo <guillermoberna_@hotmail.com>
2022-08-11 09:43:42 -0400

    first commit

---
+++ hola_mundo.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hola Mundo uwu";
+
+//esto es practica de PHP, nunca lo habia usado XD 
+//codigo dentro del navegador no puede ser visto
+
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1><?php echo "HOLA MUNDO ESTO ES UN H1"?></h1>
+</body>
+</html>
